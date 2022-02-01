02ce84389f0bf4f6edf3c223f12bc832c0c90601
Arthur L-Brjc <a.lambert-bourjac@octo.com>
2022-02-01 16:54:37 +0100

    fix: ignore unknown fields when parsing users

--- keycloak-customisation/src/main/java/passemploi/authentication/user/model/UtilisateurSsoPeJeune.java
+++ keycloak-customisation/src/main/java/passemploi/authentication/user/model/UtilisateurSsoPeJeune.java
@@ -1,10 +1,12 @@
 package passemploi.authentication.user.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UtilisateurSsoPeJeune {
     private String given_name;
     private String family_name;
