dc8c56f98b1092c271041cd30d60aba34b3ab52f
Jozo <jozo.robert@gmail.com>
2022-02-28 10:50:27 +0100

    feat: ajouter les roles pour tous

--- keycloak-customisation/src/main/java/passemploi/authentication/user/model/Utilisateur.java
+++ keycloak-customisation/src/main/java/passemploi/authentication/user/model/Utilisateur.java
@@ -1,9 +1,13 @@
 package passemploi.authentication.user.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -16,5 +20,14 @@ public class Utilisateur {
     private String email;
     private Structure structure;
     private Type type;
+    @Getter(AccessLevel.NONE)
     private List<String> roles;
+
+    // TODO: enlever après avoir décomissionné les rôles realm
+    public List<String> getRoles() {
+        if (roles == null) {
+            return Collections.emptyList();
+        }
+        return roles;
+    }
 }
