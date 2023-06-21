40793e6ad7b1eb2d1d49bcef314fea702761b3bb
Malek Zemni <malek.zemni@octo.com>
2023-06-21 17:13:04 +0200

    feat: add token exchange similo-jeune

--- config/terraform/token-exchange.tf
+++ config/terraform/token-exchange.tf
@@ -22,3 +22,11 @@ resource "keycloak_identity_provider_token_exchange_scope_permission" "token_exc
     keycloak_openid_client.keycloak_openid_client_api.id
   ]
 }
+resource "keycloak_identity_provider_token_exchange_scope_permission" "token_exchange_milo_jeune_api" {
+  realm_id       = keycloak_realm.pass-emploi.id
+  provider_alias = keycloak_oidc_identity_provider.idp_milo_jeune.alias
+  policy_type    = "client"
+  clients = [
+    keycloak_openid_client.keycloak_openid_client_api.id
+  ]
+}
